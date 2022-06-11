0d12c5ff34f956ed0d3d66eea85f3f8c528bcb5d
Dawid Lachowicz <domdaw7@gmail.com>
2022-06-11 21:27:34 +0100

    Add checks for failed insertion/deletion

--- src/main/kotlin/io/github/dawidl022/models/util/SeedableTable.kt
+++ src/main/kotlin/io/github/dawidl022/models/util/SeedableTable.kt
@@ -16,7 +16,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.statements.InsertStatement
 
-abstract class SeedableTable<T: Idable>(val name: String) : Table() {
+abstract class SeedableTable<T : Idable>(val name: String) : Table() {
     abstract fun seed(): List<T>
     abstract fun <Key : Any> insertSchema(insert: InsertStatement<Key>, item: T)
     abstract fun fromRow(row: ResultRow): T
@@ -33,34 +33,44 @@ abstract class SeedableTable<T: Idable>(val name: String) : Table() {
             select {
                 this@SeedableTable.id eq queryId.toInt()
             }.map(::fromRow).firstOrNull()
-        } ?:
-        return notFoundResponse(call, name, queryId)
+        } ?: return notFoundResponse(call, name, queryId)
         call.respond(response(resource))
     }
 
     suspend inline fun <reified E : T> add(call: ApplicationCall) {
         val item = call.receive<E>()
-        transaction {
+        val insertedCount = transaction {
             insert {
                 insertSchema(it, item)
             }
+        }.insertedCount
+
+        if (insertedCount > 0) {
+            call.respondText(
+                "Added $name.",
+                status = HttpStatusCode.Accepted
+            )
+        } else {
+            call.respondText(
+                "Failed to add $name",
+                status = HttpStatusCode.InternalServerError
+            )
         }
-        call.respondText(
-            "Added $name.",
-            status = HttpStatusCode.Accepted
-        )
     }
 
     suspend fun delete(call: ApplicationCall) {
-        get(call) {
-            transaction {
-                deleteWhere { this@SeedableTable.id eq it.id!! }
-            }
-            TextContent(
+        val queryId = call.parameters["id"] ?: return noIdResponse(call)
+        val deletedCount = transaction {
+            deleteWhere { this@SeedableTable.id eq queryId.toInt() }
+        }
+
+        if (deletedCount > 0) {
+            call.respondText(
                 "${name.replaceFirstChar(Char::uppercase)} removed.",
                 status = HttpStatusCode.Accepted,
-                contentType = ContentType.Text.Plain
             )
+        } else {
+            notFoundResponse(call, name, queryId)
         }
     }
 }
